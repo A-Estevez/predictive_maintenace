4b42da3c1007f3c7b16bf2bd50b4c0527204f9ed
A-Estevez <52750278+A-Estevez@users.noreply.github.com>
2021-09-17 18:33:06 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Test to see if I can make a predictive maintenance work for a company',
+    author='Adrián E',
+    license='MIT',
+)
